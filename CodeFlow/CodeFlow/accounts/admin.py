b1a8db898e04771b325b7ead739eb8a3458a762f
gmtev <gmtev1007@gmail.com>
2024-11-19 23:07:36 +0200

    added backends authentication and like, comment and question models + admin for the user model
    Added the custom authentication for the profile, allowing login with either the username or the email, as well as the models for the likes, comments and questions; registered the CustomUserAdmin.

--- CodeFlow/CodeFlow/accounts/admin.py
+++ CodeFlow/CodeFlow/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from CodeFlow.accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class CustomUserAdmin(UserAdmin):
+    model = UserModel
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    list_display = ('pk', 'email', 'username', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('pk',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
--- CodeFlow/CodeFlow/content/models.py
+++ CodeFlow/CodeFlow/content/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.core.validators import MinLengthValidator
+from CodeFlow.validators import ImageSizeValidator
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Question(models.Model):
+    MAX_QUESTION_IMAGE_SIZE = 5
+    MIN_QUESTION_TITLE_SIZE = 5
+    MAX_QUESTION_TITLE_SIZE = 100
+
+    question_title = models.CharField(
+        max_length=MAX_QUESTION_TITLE_SIZE,
+        validators=[MinLengthValidator(MIN_QUESTION_TITLE_SIZE)],
+    )
+
+    text = models.TextField()
+    profile_picture = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='',
+        validators=[
+            ImageSizeValidator(MAX_QUESTION_IMAGE_SIZE),
+        ],
+    )
+    slug = models.SlugField(
+        null=True,
+        blank=True,
+        unique=True,
+        editable=False,
+    )
+
+    user = models.ForeignKey(
+        to=UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.question_title}-{self.id}")
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.question_title
