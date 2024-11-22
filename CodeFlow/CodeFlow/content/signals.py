1fc655e47bc21283d55c72905c4a104365a5aa37
gmtev <gmtev1007@gmail.com>
2024-11-22 23:07:41 +0200

    Delete signals.py

--- CodeFlow/CodeFlow/content/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.utils.text import slugify
-from CodeFlow.content.models import Lecture
-
-@receiver(pre_save, sender=Lecture)
-def generate_slug(sender, instance, **kwargs):
-    if not instance.slug:
-        instance.slug = slugify(f"{instance.lecture_title}-{instance.id}")
