4769e56c9aea8c7a01e825b81f2371b50f8917e2
gmtev <145558375+gmtev@users.noreply.github.com>
2024-12-10 19:43:20 +0200

    included the old version of the content/urls as a comment in the code

--- CodeFlow/CodeFlow/content/urls.py
+++ CodeFlow/CodeFlow/content/urls.py
@@ -32,3 +32,26 @@ urlpatterns = [
         ])),
     ])),
 ]
+
+# a more readable version of this: urlpatterns = [
+    # path('questions/', include([
+        # path('', QuestionListView.as_view(), name='questions'),
+        # path('create/', QuestionCreateView.as_view(), name='question-create'),
+        # path('<slug:slug>/', QuestionDetailView.as_view(), name='question-details'),
+        # path('<slug:slug>/edit/', QuestionEditView.as_view(), name='question-edit'),
+        # path('<slug:slug>/delete/', QuestionDeleteView.as_view(), name='question-delete'),
+    # ])),
+# 
+    # path('lectures/', include([
+        # path('', LectureListView.as_view(), name='lectures'),
+        # path('create/', LectureCreateView.as_view(), name='lecture-create'),
+        # path('<slug:slug>/', LectureDetailView.as_view(), name='lecture-details'),
+        # path('<slug:slug>/edit/', LectureEditView.as_view(), name='lecture-edit'),
+        # path('<slug:slug>/delete/', LectureDeleteView.as_view(), name='lecture-delete'),
+        # path('<slug:slug>/create/', SectionCreateView.as_view(), name='section-create'),
+        # path('<slug:slug>/<int:pk>/', SectionDetailView.as_view(), name='section-details'),
+        # path('<slug:slug>/<int:pk>/edit/', SectionEditView.as_view(), name='section-edit'),
+        # path('<slug:slug>/<int:pk>/delete/', SectionDeleteView.as_view(), name='section-delete'),
+# 
+    # ])),
+# ]
